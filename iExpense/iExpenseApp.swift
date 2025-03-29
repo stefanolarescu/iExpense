1ece9301e3b7ff3a1759a39964d11db1b5563835
Stefan Olarescu <stefanolarescu@gmail.com>
2025-03-29 14:01:01 +0200

    Created Xcode project.

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Stefan Olarescu on 29.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
